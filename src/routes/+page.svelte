0d9d5d7f6f50e11a0ced3dda145c221103bbf66b
b1uejay <hi@b1uejay.dev>
2022-11-05 17:19:13 -0500

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<h1>Hi there!</h1>
+<p>I'm b1uejay (aka Jay) and I'm a student and programmer from the United States who's interested in pretty much anything to do with technology and birds. I'm currently exploring the worlds of software development, IT, and cybersecurity, though I'm also very interested in exploring web development (as you can see I'm currently bad at it) and networking. You can reach out to me on Discord at b1uejay#5097 or through email at <a href="mailto:hi@b1uejay.dev">hi@b1uejay.dev</a>.</p>
+<p>Discord: b1uejay#5097</p>
+
+<a href="https://github.com/b1uejay2">GitHub</a>
+<a href="https://keybase.io/b1uejay">Keybase</a>
