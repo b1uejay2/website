009bd45676af22bdc9a7c40bcd96d75d1e038985
b1uejay <hi@b1uejay.dev>
2022-12-18 18:36:44 -0600

    update to sveltekit v1, add a bunch of stuff

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,17 @@
 <h1>Hi there!</h1>
-<p>I'm b1uejay (aka Jay) and I'm a student and programmer from the United States who's interested in pretty much anything to do with technology and birds. I'm currently exploring the worlds of software development, IT, and cybersecurity, though I'm also very interested in exploring web development (as you can see I'm currently bad at it) and networking.
-<p>Discord: b1uejay#5097</p>
+<p>I'm b1uejay (aka Jay) and I'm an 18 year old student and programmer from the United States who's interested in pretty much anything to do with technology and birds. I'm currently exploring the worlds of software development, cybersecurity, and IT though I'm also very interested in exploring web development (as you can see I'm currently bad at it) and networking.
+<p>Pronouns: he/they</p>
 
-<a href="https://github.com/b1uejay2">GitHub</a>
-<a href="mailto:hi@b1uejay.dev">Email</a>
-<a href="https://keybase.io/b1uejay">Keybase</a>
+<h2>Skills</h2>
+<a href="https://skillicons.dev">
+    <img src="https://skillicons.dev/icons?i=bash,cloudflare,discord,bots,docker,fastapi,git,github,linux,md,nginx,postgres,py,raspberrypi,vscode&theme=light" alt="Skill Icons" />
+</a>
+
+<h2>Links</h2>
+<a href="https://discord.com/users/554807126174990349">b1uejay#5097 @ Discord<br></a>
+<a href="https://github.com/b1uejay2">b1uejay2 @ GitHub<br></a>
+<a href="https://gitlab.com/b1uejay">b1uejay @ GitLab<br></a>
+<a href="https://hackerone.com/b1uejay">b1uejay @ HackerOne<br></a>
+<a href="https://keybase.io/b1uejay">b1uejay @ Keybase<br></a>
+<a href="https://ko-fi.com/b1uejay">b1uejay @ Ko-Fi<br></a>
+<a href="mailto:hi@b1uejay.dev">Email<br></a>
